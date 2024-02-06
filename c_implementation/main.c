24726c5643aac446ff00252bce61cd82a0b53946
LucasRxTx <lucasrxtx@lucasscott.com>
2024-02-06 09:44:47 +0100

    c_implementation: Simplifies logic for removing three moves in a row

--- c_implementation/main.c
+++ c_implementation/main.c
@@ -34,53 +34,32 @@ int main() {
     size_t move_buffer_index = 0;
     char move_buffer[255] = {0};
 
-    while (1) {
-        int ret = yahdlc_get_data(&control, frame_buffer + state, bytes_read - state, frame_data, &data_length);
-        if (ret < 0) {
-            break;
+        while (1) {
+            int ret = yahdlc_get_data(&control, frame_buffer + state, bytes_read - state, frame_data, &data_length);
+            if (ret < 0)
+                break;
+
+            if (control.frame == YAHDLC_FRAME_DATA)
+                move_buffer[move_buffer_index++] = frame_data[0];
+
+            // Move to the next frame
+            state += ret;
         }
-
-        if (control.frame == YAHDLC_FRAME_DATA)
-            move_buffer[move_buffer_index++] = frame_data[0];
-
-        // Move to the next frame
-        state += ret;
-    }
 
     player_t player = {{0, 4}, 'x'};
     int filtered_moves[MAX_FILTERED_MOVES];
     int filtered_moves_index = 0;
 
-    int same_move_buffer[MAX_SAME_MOVE_COUNT] = {0};
-    int same_move_buffer_index = 0;
     int move = 0;
 
     // filter out moves that occured 3 times in a row
     for (size_t i = 0; i < move_buffer_index; i++) {
         move = move_buffer[i];
-        if (is_same_move(move)) {
-            // Add to same move buffer
-            same_move_buffer[same_move_buffer_index++] = move;
-            if (same_move_buffer_index == MAX_SAME_MOVE_COUNT) {
-                // if bufer is full, reset it
-                same_move_buffer_index = 0;
-            }
+
+        if (i < move_buffer_index - 2 && move == move_buffer[i + 1] && move == move_buffer[i + 2])
             continue;
-        }
 
-        // Exaust same moves buffer
-        for (size_t j = 0; j < same_move_buffer_index; j++) {
-            filtered_moves[filtered_moves_index++] = same_move_buffer[j];
-        }
-
-        // Add current move to same move buffer
-        same_move_buffer_index = 0;
-        same_move_buffer[same_move_buffer_index++] = move;
-    }
-
-    // Consume last moves
-    for (size_t j = 0; j < same_move_buffer_index; j++) {
-        filtered_moves[filtered_moves_index++] = same_move_buffer[j];
+        filtered_moves[filtered_moves_index++] = move;
     }
 
     // Move the player
